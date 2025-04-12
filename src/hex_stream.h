7369e679cd635db6355af438fe471a8a86c6f936
EdvardsF <francs.edvards@inbox.lv>
2025-04-12 15:18:22 +0300

    Implement header reader

--- src/hex_stream.h
+++ src/hex_stream.h
@@ -8,16 +8,13 @@ struct hexStream {
     std::string buffer;
     size_t bufferSize;
 
-    // Constructor
     hexStream(std::string hex_data);
 
-    // Public methods
     uint8_t readByte();
     uint32_t read4_le();
     uint32_t read4_be();
 
 private:
-    // Internal helpers
     size_t m_getBufferSize();
     bool isEnoughSize(size_t size);
 };
