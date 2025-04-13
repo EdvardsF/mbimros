ab30a6bc18f16adf686178152f229391469580f1
EdvardsF <francs.edvards@inbox.lv>
2025-04-13 09:29:37 +0300

    test for close and open messages

--- tests/test_mbim.cpp
+++ tests/test_mbim.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include "catch.h"
 
 #include "../src/mbim_decoder.h"
 #include "../src/hex_stream.h"
@@ -23,4 +23,24 @@ TEST_CASE("MBIM_FRAGMENT_HEADER parses fields correctly") {
 
     REQUIRE(msg.TOTAL_FRAGMENTS.value == 1);
     REQUIRE(msg.CURRENT_FRAGMENT.value == 0);
+}
+
+TEST_CASE("MBIM_OPEN_MSG parses fields correctly") {
+    std::string msg_header = "000000000000000000000000";
+    std::string payload = "50000000";
+    std::string to_test = msg_header + payload;
+    hexStream hs(to_test);
+
+    MBIM_OPEN_MSG msg(hs);
+
+    REQUIRE(msg.MAX_CONTROL_TRANSFER.value == 80);
+}
+
+TEST_CASE("MBIM_CLOSE_MSG parses fields correctly") {
+    std::string msg_header = "020000000000000000000000";
+    hexStream hs(msg_header);
+
+    MBIM_CLOSE_MSG msg(hs);
+
+    REQUIRE(msg.MESSAGE_HEADER.MESSAGE_TYPE.value == MESSAGE_TYPE_ENUM::MBIM_CLOSE_MSG);
 }
