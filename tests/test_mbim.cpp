1813ba3ad8fff4d411a8493f796403083aa3b398
EdvardsF <francs.edvards@inbox.lv>
2025-04-13 13:49:32 +0300

    add test case for MBIM_HOST_ERROR_MSG

--- tests/test_mbim.cpp
+++ tests/test_mbim.cpp
@@ -46,7 +46,7 @@ TEST_CASE("MBIM_CLOSE_MSG parses fields correctly") {
     REQUIRE(msg.MESSAGE_HEADER.MESSAGE_TYPE.value == MESSAGE_TYPE_ENUM::MBIM_CLOSE_MSG);
 }
 
-TEST_CASE("MBIM_HOST_ERROR_MSG parses fields correctly") {
+TEST_CASE("MBIM_COMMAND_MSG parses fields correctly") {
     std::string msg_header = "010000003400000015000000";
     std::string msg_fragment_header = "0100000000000000";
     std::string payload = "a289cc33bcbb8b4fb6b0133ec2aae6df01000000000000000400000011111111";
@@ -60,4 +60,15 @@ TEST_CASE("MBIM_HOST_ERROR_MSG parses fields correctly") {
     REQUIRE(msg.COMMAND_TYPE.value == MESSAGE_QUERY_OR_SET_ENUM::QUERY);
     REQUIRE(msg.INFORMATION_BUFFER_LENGTH.value == 4);
     REQUIRE(msg.INFORMATION_BUFFER.value == "11111111");
+}
+
+TEST_CASE("MBIM_HOST_ERROR_MSG parses fields correctly") {
+    std::string msg_header = "010000003400000015000000";
+    std::string payload = "03000000";
+    std::string to_test = msg_header + payload;
+    hexStream hs(to_test);
+
+    MBIM_HOST_ERROR_MSG msg(hs);
+
+    REQUIRE(msg.ERROR_STATUS_CODE.value == MBIM_PROTOCOL_ERROR_CODES_ENUM::MBIM_ERROR_LENGTH_MISMATCH);
 }
