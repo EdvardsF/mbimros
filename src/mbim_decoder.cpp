a9acdd61871a06cf591c644c98b4ea033a0008f9
EdvardsF <francs.edvards@inbox.lv>
2025-04-13 11:22:50 +0300

    refactor mbim_decoder

--- src/mbim_decoder.cpp
+++ src/mbim_decoder.cpp
@@ -6,20 +6,21 @@
 MBIM_MESSAGE_HEADER::MBIM_MESSAGE_HEADER(hexStream& hs) {
     MESSAGE_TYPE.bind(this);
     MESSAGE_TYPE.setFormatter(map_type);
+    MESSAGE_TYPE.set(static_cast<MESSAGE_TYPE_ENUM>(hs.read4_le()));
 
     MESSAGE_LENGTH.bind(this);
-    TRANSACTION_ID.bind(this);
-
-    MESSAGE_TYPE.set(static_cast<MESSAGE_TYPE_ENUM>(hs.read4_le()));
     MESSAGE_LENGTH.set(hs.read4_le());
+
+    TRANSACTION_ID.bind(this);
     TRANSACTION_ID.set(hs.read4_le());
 }
 
 
 MBIM_FRAGMENT_HEADER::MBIM_FRAGMENT_HEADER(hexStream& hs) {
     TOTAL_FRAGMENTS.bind(this);
-    CURRENT_FRAGMENT.bind(this);
     TOTAL_FRAGMENTS.set(hs.read4_le());
+
+    CURRENT_FRAGMENT.bind(this);
     CURRENT_FRAGMENT.set(hs.read4_le());
 }
 
