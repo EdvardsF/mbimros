30498c2c81d915e2b967f49213540e7f7ee82ba4
EdvardsF <francs.edvards@inbox.lv>
2025-04-20 10:16:47 +0300

    refactor: Use binding helpers in messages.cpp

--- src/protocol/v1/messages.cpp
+++ src/protocol/v1/messages.cpp
@@ -6,33 +6,25 @@
 
 MBIM_MESSAGE_HEADER::MBIM_MESSAGE_HEADER(hexStream& hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
-    MESSAGE_TYPE.bind(this);
-    MESSAGE_TYPE.setEnumFormatter(map_type);
-    MESSAGE_TYPE.set(static_cast<MESSAGE_TYPE_ENUM>(hs.readUint32LE()));
 
-    MESSAGE_LENGTH.bind(this);
-    MESSAGE_LENGTH.set(hs.readUint32LE());
-
-    TRANSACTION_ID.bind(this);
-    TRANSACTION_ID.set(hs.readUint32LE());
+    bindFormatSet(MESSAGE_TYPE, this, map_type, hs.readUint32LE());
+    bindSimpleSet(MESSAGE_LENGTH, this, hs.readUint32LE());
+    bindSimpleSet(TRANSACTION_ID, this, hs.readUint32LE());
 }
 
 
 MBIM_FRAGMENT_HEADER::MBIM_FRAGMENT_HEADER(hexStream& hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
-    TOTAL_FRAGMENTS.bind(this);
-    TOTAL_FRAGMENTS.set(hs.readUint32LE());
 
-    CURRENT_FRAGMENT.bind(this);
-    CURRENT_FRAGMENT.set(hs.readUint32LE());
+    bindSimpleSet(TOTAL_FRAGMENTS, this, hs.readUint32LE());
+    bindSimpleSet(CURRENT_FRAGMENT, this, hs.readUint32LE());
 }
 
 
 MBIM_OPEN_MSG::MBIM_OPEN_MSG(hexStream& hs) : MESSAGE_HEADER(hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
     includeHeader(&MESSAGE_HEADER);
-    MAX_CONTROL_TRANSFER.bind(this);
-    MAX_CONTROL_TRANSFER.set(hs.readUint32LE());
+    bindSimpleSet(MAX_CONTROL_TRANSFER, this, hs.readUint32LE());
 }
 
 MBIM_CLOSE_MSG::MBIM_CLOSE_MSG(hexStream& hs) : MESSAGE_HEADER(hs) {
@@ -49,16 +41,9 @@ MBIM_COMMAND_MSG::MBIM_COMMAND_MSG(hexStream& hs) : MESSAGE_HEADER(hs), FRAGMENT
     DEVICE_SERVICE_ID.setStringFormatter(map_uuid);
     DEVICE_SERVICE_ID.set(hs.readHexBytes(16));
 
-    CID.bind(this);
-    CID.setNumberFormatter(get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value));
-    CID.set(hs.readUint32LE());
-
-    COMMAND_TYPE.bind(this);
-    COMMAND_TYPE.setEnumFormatter(map_query_or_set);
-    COMMAND_TYPE.set(static_cast<MESSAGE_QUERY_OR_SET_ENUM>(hs.readUint32LE()));
-
-    INFORMATION_BUFFER_LENGTH.bind(this);
-    INFORMATION_BUFFER_LENGTH.set(hs.readUint32LE());
+    bindFormatSet(CID, this, get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value), hs.readUint32LE());
+    bindFormatSet(COMMAND_TYPE, this, map_query_or_set, hs.readUint32LE());
+    bindSimpleSet(INFORMATION_BUFFER_LENGTH, this, hs.readUint32LE());
 
     BufferDirection direction = COMMAND_TYPE.value == MESSAGE_QUERY_OR_SET_ENUM::QUERY ? BufferDirection::HostToModemQuery : BufferDirection::HostToModemSet;
     std::unique_ptr<informationBuffer> buffer = findBuffer(buffer_registry, DEVICE_SERVICE_ID.value, CID.value, direction);
@@ -69,25 +54,19 @@ MBIM_COMMAND_MSG::MBIM_COMMAND_MSG(hexStream& hs) : MESSAGE_HEADER(hs), FRAGMENT
 MBIM_HOST_ERROR_MSG::MBIM_HOST_ERROR_MSG(hexStream& hs) : MESSAGE_HEADER(hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
     includeHeader(&MESSAGE_HEADER);
-    ERROR_STATUS_CODE.bind(this);
-    ERROR_STATUS_CODE.setEnumFormatter(map_host_error);
-    ERROR_STATUS_CODE.set(static_cast<MBIM_PROTOCOL_ERROR_CODES_ENUM>(hs.readUint32LE()));
+    bindFormatSet(ERROR_STATUS_CODE, this, map_host_error, hs.readUint32LE());
 }
 
 MBIM_OPEN_DONE::MBIM_OPEN_DONE(hexStream& hs) : MESSAGE_HEADER(hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
     includeHeader(&MESSAGE_HEADER);
-    STATUS.bind(this);
-    STATUS.setEnumFormatter(map_host_status);
-    STATUS.set(static_cast<MBIM_STATUS_TO_HOST_ENUM>(hs.readUint32LE()));
+    bindFormatSet(STATUS, this, map_host_status, hs.readUint32LE());
 }
 
 MBIM_CLOSE_DONE::MBIM_CLOSE_DONE(hexStream& hs) : MESSAGE_HEADER(hs) {
     HexStreamAlignmentGuard _(hs); // Checks for 4 byte alignment
     includeHeader(&MESSAGE_HEADER);
-    STATUS.bind(this);
-    STATUS.setEnumFormatter(map_host_status);
-    STATUS.set(static_cast<MBIM_STATUS_TO_HOST_ENUM>(hs.readUint32LE()));
+    bindFormatSet(STATUS, this, map_host_status, hs.readUint32LE());
 }
 
 MBIM_COMMAND_DONE::MBIM_COMMAND_DONE(hexStream& hs) : MESSAGE_HEADER(hs), FRAGMENT_HEADER(hs) {
@@ -95,20 +74,14 @@ MBIM_COMMAND_DONE::MBIM_COMMAND_DONE(hexStream& hs) : MESSAGE_HEADER(hs), FRAGME
     includeHeader(&MESSAGE_HEADER);
     includeFragmentHeader(&FRAGMENT_HEADER);
 
+    // update bindFormatSet method to handle UUID
     DEVICE_SERVICE_ID.bind(this);
     DEVICE_SERVICE_ID.setEnumFormatter(map_uuid);
     DEVICE_SERVICE_ID.set(hs.readHexBytes(16));
 
-    CID.bind(this);
-    CID.setNumberFormatter(get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value));
-    CID.set(hs.readUint32LE());
-
-    STATUS.bind(this);
-    STATUS.setEnumFormatter(map_host_status);
-    STATUS.set(static_cast<MBIM_STATUS_TO_HOST_ENUM>(hs.readUint32LE()));
-
-    INFORMATION_BUFFER_LENGTH.bind(this);
-    INFORMATION_BUFFER_LENGTH.set(hs.readUint32LE());
+    bindFormatSet(CID, this, get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value), hs.readUint32LE());
+    bindFormatSet(STATUS, this, map_host_status, hs.readUint32LE());
+    bindSimpleSet(INFORMATION_BUFFER_LENGTH, this, hs.readUint32LE());
 
     std::unique_ptr<informationBuffer> buffer = findBuffer(buffer_registry, DEVICE_SERVICE_ID.value, CID.value, BufferDirection::ModemToHostResponse);
     buffer->parse(hs);
@@ -124,12 +97,8 @@ MBIM_INDICATE_STATUS_MSG::MBIM_INDICATE_STATUS_MSG(hexStream& hs) : MESSAGE_HEAD
     DEVICE_SERVICE_ID.setStringFormatter(map_uuid);
     DEVICE_SERVICE_ID.set(hs.readHexBytes(16));
 
-    CID.bind(this);
-    CID.setNumberFormatter(get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value));
-    CID.set(hs.readUint32LE());
-
-    INFORMATION_BUFFER_LENGTH.bind(this);
-    INFORMATION_BUFFER_LENGTH.set(hs.readUint32LE());
+    bindFormatSet(CID, this, get_cid_mapper_for_uuid(DEVICE_SERVICE_ID.value), hs.readUint32LE());
+    bindSimpleSet(INFORMATION_BUFFER_LENGTH, this, hs.readUint32LE());
 
     std::unique_ptr<informationBuffer> buffer = findBuffer(buffer_registry, DEVICE_SERVICE_ID.value, CID.value, BufferDirection::ModemToHostIndication);
     buffer->parse(hs);
